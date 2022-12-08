9efa2c6bd1856c46bb26f3b48ac68c67d5397a7e
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-12-08 01:40:53 +0100

    analysis.convex.strict

--- src/analysis/convex/strict.lean
+++ src/analysis/convex/strict.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 -/
 import analysis.convex.basic
+import topology.algebra.group.basic
 import topology.algebra.order.basic
 
 /-!
