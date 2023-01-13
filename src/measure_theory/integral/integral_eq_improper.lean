610569817d621dd2f9c490ceaf591a47b998a233
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-01-13 17:18:15 -0500

    measure_theory.integral.integral_eq_improper

--- src/measure_theory/integral/integral_eq_improper.lean
+++ src/measure_theory/integral/integral_eq_improper.lean
@@ -3,6 +3,7 @@ Copyright (c) 2021 Anatole Dedecker. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Anatole Dedecker, Bhavik Mehta
 -/
+import analysis.special_functions.pow_deriv
 import measure_theory.integral.interval_integral
 import order.filter.at_top_bot
 import measure_theory.function.jacobian
