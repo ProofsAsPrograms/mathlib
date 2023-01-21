defc94324dc076e9063e192ef7079e0e67bbb0d6
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-01-21 00:59:26 -0500

    measure_theory.integral.inner

--- src/measure_theory/integral/inner.lean
+++ src/measure_theory/integral/inner.lean
@@ -3,22 +3,20 @@ import measure_theory.integral.set_integral
 
 namespace measure_theory
 
-variables {α β E F : Type*} [measurable_space α]
+variables {α : Type*} [measurable_space α]
 
-variables {μ : measure α} {𝕜 : Type*} [is_R_or_C 𝕜] [normed_space 𝕜 E]
-  [normed_add_comm_group F] [normed_space 𝕜 F]
-  {p : ennreal}
+variables {μ : measure α} {𝕜 : Type*} [is_R_or_C 𝕜]
 
-variables {E' : Type*} [inner_product_space 𝕜 E'] [complete_space E']
+variables {E : Type*} [inner_product_space 𝕜 E] [complete_space E] [normed_space ℝ E]
 
-local notation `⟪`x`, `y`⟫` := @inner 𝕜 E' _ x y
+local notation `⟪`x`, `y`⟫` := @inner 𝕜 E _ x y
 
-lemma integral_inner {f : α → E'} (hf : integrable f μ) (c : E') :
+lemma integral_inner {f : α → E} (hf : integrable f μ) (c : E) :
   ∫ x, ⟪c, f x⟫ ∂μ = ⟪c, ∫ x, f x ∂μ⟫ :=
-((@innerSL 𝕜 E' _ _ c).restrict_scalars ℝ).integral_comp_comm hf
+((@innerSL 𝕜 E _ _ c).restrict_scalars ℝ).integral_comp_comm hf
 
-lemma integral_eq_zero_of_forall_integral_inner_eq_zero (f : α → E') (hf : integrable f μ)
-  (hf_int : ∀ (c : E'), ∫ x, ⟪c, f x⟫ ∂μ = 0) :
+lemma integral_eq_zero_of_forall_integral_inner_eq_zero (f : α → E) (hf : integrable f μ)
+  (hf_int : ∀ (c : E), ∫ x, ⟪c, f x⟫ ∂μ = 0) :
   ∫ x, f x ∂μ = 0 :=
 by { specialize hf_int (∫ x, f x ∂μ), rwa [integral_inner hf, inner_self_eq_zero] at hf_int }
 
